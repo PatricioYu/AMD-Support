0d4b858d5d31dbf5c2099f1d1681cda0fd3f0823
Yu Patricio <yupatricio0@gmail.com>
2021-10-25 22:20:14 -0300

    added UserSchema

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const port = process.env.PORT || 3000;
+const db = process.env.MONGODB_URI || 'mongodb://localhost/amd-support';
+
+const app = express();
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+  .catch(err => console.error(`Connection error ${err}`));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+});
