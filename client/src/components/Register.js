79c208024c3ab267886df4c546ad0b41b3f40b59
xoan <facutoto666@gmail.com>
2021-11-09 14:23:23 -0300

    link interfaz de usuario

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,19 +1,29 @@
 import './Register.css';
 import amdpng from './amdpng.png';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 
 function Register(){
+
+    const history = useHistory();
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log('form enviado');
+      history.push('/usuario');
+
+    };
+
     return(
     
     <div className="div_form">
-        <form action="" className="formulario">
+        <form onSubmit={handleSubmit} className="formulario">
         <h1 className="formulario__titulo">Register</h1>
 
         <label for="" className="formulario__label">Username</label>
         <input type="text" className="formulario__input"/>
         <label for="" className="formulario__label">Password</label>
         <input type="password" className="formulario__input"/>
-        <input type="submit" value="Submit" className="formulario__submit" />
+        <input type="submit" value="Submit" className="formulario__submit"/> 
         
         <div class="botonazul">
             <p><Link to="/" className="cuenta" >Go Back</Link></p>
