a3889cb77eb50295ebdfb997f6fbb81ebc203efb
Patricio Yu <yupatricio0@gmail.com>
2021-11-09 14:46:31 -0300

    routes

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,26 +1,30 @@
+
 import './Register.css';
+import React, { useState } from 'react';
 import amdpng from './amdpng.png';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Register(){
-    return(
+    const [users, setUser] = useState([]);
     
-    <div className="div_form">
-        <form action="" className="formulario">
-        <h1 className="formulario__titulo">Register</h1>
-
-        <label for="" className="formulario__label">Username</label>
-        <input type="text" className="formulario__input"/>
-        <label for="" className="formulario__label">Password</label>
-        <input type="password" className="formulario__input"/>
-        <input type="submit" value="Submit" className="formulario__submit" />
+    return(
         
-        <div class="botonazul">
-            <p><Link to="/" className="cuenta" >Go Back</Link></p>
-            <p><Link to="./login" className="back">Already have an account?</Link></p>
+        <div className="div_form">
+            <form onSubmit={handleSubmit} action="" className="formulario">
+            <h1 className="formulario__titulo">Register</h1>
+
+            <label for="" className="formulario__label">Username</label>
+            <input type="text" className="formulario__input"/>
+            <label for="" className="formulario__label">Password</label>
+            <input type="password" className="formulario__input"/>
+            <input type="submit" value="Submit" className="formulario__submit"/>
+            
+            <div class="botonazul">
+                <p><Link to="/" className="cuenta" >Go Back</Link></p>
+                <p><Link to="./login" className="back">Already have an account?</Link></p>
+            </div>
+            </form>
         </div>
-        </form>
-    </div>
     
     );
 }
--- models/User.js
+++ models/User.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: String,
-  password: String
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
 const User = mongoose.model('User', UserSchema);
