5d0952453138a114805ab1a3209f20ddc1cd12aa
Valentino Salto <saltovalentino@gmail.com>
2021-11-09 22:25:26 -0300

    posibles botones/prueba

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Div_image from './components/Div_image';
 import Register from './components/Register';
 import Login from './components/Login';
+import Usuario from './components/Usuario';
 import { BrowserRouter as Router, Switch,Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -22,8 +23,8 @@ ReactDOM.render(
       </Route>
       
       <Route path="/usuario">
-        <header_usua/>
-        <body_usua/>
+        <Usuario/>
+        
       </Route>
 
       <Route path="/">
