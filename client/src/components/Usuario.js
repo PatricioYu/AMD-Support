243229d6026cb218c27c49171f9322088ec2a9f7
Valentino Salto <saltovalentino@gmail.com>
2021-11-10 01:28:45 -0300

    Cambio de color(testeo) y link de boton salir(interfaz de usuario).

--- client/src/components/Usuario.js
+++ client/src/components/Usuario.js
@@ -1,17 +1,28 @@
-import './Header.css';
+import './Usuario.css';
 import logoAmd from './logoAmd.png';
 import gente from './gente.png'
-
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 
 function Usuario(){
   
+  const history = useHistory();
+
+  const onClick = e => {
+    e.preventDefault();
+    console.log('Has salido');
+    history.push('/Register');
+
+  };
+
+
     return ( 
-            <div className="header">
-              <div className="icon__div"><img src={gente} className="gente" id="genteicon" alt="icono de gente"/></div>
+            <div className="header_usuario">
+              <img src={gente} className="logo" id="headerLogo" alt="Logo del Header" />
               <div className="verticleLine"></div>
               <div className="links"><button className="css-button-neumorphic">Shop</button><button className="css-button-neumorphic">Accessories</button><button className="css-button-neumorphic">ODA</button>
               <button className="css-button-neumorphic">Options</button>
               </div>
+              <button className="css-button-neumorphic-salir" onClick={onClick}>Salir</button>
             </div> 
     );
     
