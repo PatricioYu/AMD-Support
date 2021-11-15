e48f772d8269b10222949186012516fbda74a3bc
patricio yu <yupatricio0@gmail.com>
2021-11-15 14:39:39 -0300

    ruta user

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ const app = express();
 
 app.use(express.static('public'));
 
-// mongoose.set('useUnifiedTopology', true);
-// mongoose.set('useFindAndModify', false);
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/', require('./routes/user'));
+// 5to middleware (error 404 not found)
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+// con el error 404 o 500 si vino de otro lado el problema
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  // para mas detalles usar: console.error(err.stack)
+  res.json({ error: err.message });
+});
+
 mongoose
   .connect(db, { useNewUrlParser: true})
   .then(() => {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const bcrypt  = require('bcrypt');
+const router  = express.Router();
 const User    = require('../models/User');
 
 const router = express.Router();
+
+router.post('/register', (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    createdAt: req.body.createdAt
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) throw err;
+      user.password = hash;
+      user.save(err => {
+        if (err) throw err;
+        res.status(201).json(user);
+      });
+    });
+  });
+});
+router.get('/register', (req, res, next) => {
+
+});
+router.get('/register/id:', (req, res, next) => {
+
+});
+router.post('/register/id:', (req, res, next) => {
+
+});
+router.post('/register/id:', (req, res, next) => {
+
+});
 
 module.exports = router;
