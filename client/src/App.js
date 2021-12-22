e2a0349b88151b6e7d49a40e08e896a1421cbe9a
Yu Patricio <yupatricio0@gmail.com>
2021-12-22 11:05:34 -0300

    main-section responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,14 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
-
 const App = () => {
-
-    return (
-            <div className="App">
-                 <Header/>
-                 <Body />
-                 <Footer />
-            </div>
-    );
+  return (
+      <div className="App">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+  );
 };
 
 export default App;
--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -9,20 +9,20 @@ function Body() {
     return (
       <section className="mainsection">
         <div className="divA">
-        <img src={amdComponent} className="amdImg" id="amd image" alt="Imagen AMD" />
+          <img src={amdComponent} className="amdImg" id="amd image" alt="Imagen AMD" />
         </div>
 
         <div className="divB">
           <div className="divbTexto">
             <h1>AMD's Support</h1>
               <b>Sorry for all the problems! <br/> Let us know about them once you are registered.</b>
-              <Link to="/signup" class="btnNeon">
-                <span id="span1"></span>
-                <span id="span2"></span>
-                <span id="span3"></span>
-                <span id="span4"></span>
-                Sign Up
-              </Link>
+                <Link to="/signup" class="btnNeon">
+                  <span id="span1"></span>
+                  <span id="span2"></span>
+                  <span id="span3"></span>
+                  <span id="span4"></span>
+                    Sign Up
+                </Link>
           </div>
         </div>
         <span class="flecha">
